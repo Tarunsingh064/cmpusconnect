40be8954c5101f108ae0b345d245c561eb6b4ede
tarunsingh046 <tarunworkonly@gmail.com>
2025-06-30 15:45:49 +0530

    Add renderdb exturlss

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,11 +89,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL','sqlite:///db.sqlite3'),
+        conn_max_age=600,  # enables connection pooling
+    )
 }
 
 
