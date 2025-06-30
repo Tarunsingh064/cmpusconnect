830136ff24de7941f07d5dcdb1b1a014f784d30d
tarunsingh046 <tarunworkonly@gmail.com>
2025-06-30 08:36:47 +0530

    Addpostgressinteranlurlfromrender

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+
 import os
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,12 +91,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='os.getenv("DATABASE_URL"),',
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
