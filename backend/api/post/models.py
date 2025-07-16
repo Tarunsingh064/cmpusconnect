011f6f3c17cabdb9b59815bd9ab7af40a52f5ab2
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-17 05:00:53 +0530

    added cloud

--- backend/api/post/models.py
+++ backend/api/post/models.py
@@ -1,11 +1,12 @@
 # models.py
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     text = models.TextField()
-    media = models.FileField(upload_to='posts/', blank=True, null=True)
+    media = CloudinaryField('media', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
