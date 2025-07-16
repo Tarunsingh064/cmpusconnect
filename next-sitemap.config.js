cd2ae2b34e0956268ef618245f8b0c52aadcbbf3
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-16 23:21:58 +0530

    added site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://campusconnects.in',
+  siteUrl: 'https://campusconnects.in/',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
   priority: 0.7,
-  exclude: ['/Auth/*', '/admin/*', '/dashboard/*', '/api/*'], // exclude private/backend paths
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', disallow: ['/Auth', '/admin', '/dashboard'] },
-    ],
-  },
 }
