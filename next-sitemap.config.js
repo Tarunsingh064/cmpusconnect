3c3c7e3a70c7a0b501afd0c66ffafbe2af61f3cf
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-17 23:54:35 +0530

    :wq:wq:wqremove frontend and unused config files

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://campusconnects.in/',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: 'daily',
-  priority: 0.7,
-}
