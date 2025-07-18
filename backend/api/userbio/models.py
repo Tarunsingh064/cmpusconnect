00159a0a25e7d5c3094c52cf57875a9bc8534cac
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-19 03:51:55 +0530

    added media

--- backend/api/userbio/models.py
+++ backend/api/userbio/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class userbio(models.Model):
+    media = CloudinaryField('media', blank=True, null=True)
     user = models.OneToOneField('auth.user',on_delete=models.CASCADE,related_name='usebio')
     bio = models.CharField(max_length=1500,blank=True,null=True)
     college_name = models.CharField(max_length=150,blank=True,null=True)
--- backend/api/userbio/serializers.py
+++ backend/api/userbio/serializers.py
@@ -3,7 +3,21 @@ from rest_framework import serializers
 from .models import userbio
 
 class PortfolioSerializer(serializers.ModelSerializer):
+    media = serializers.ImageField(required=False, allow_null=True)
     class Meta:
         model = userbio
         fields = '__all__'
         read_only_fields = ['user']
+    
+
+    def to_representation(self, instance):
+        """ Add media URL formatting while keeping field writeable """
+        rep = super().to_representation(instance)
+        if rep.get("media"):
+            rep["media"] = rep["media"].replace("http://", "https://")
+        return rep
+
+    def get_media(self, obj):
+        if obj.media:
+            return obj.media.url.replace("http://", "https://")
+        return None
