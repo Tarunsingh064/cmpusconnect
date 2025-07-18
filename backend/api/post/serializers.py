8604e84e646a7d0afe5b75b9a39ae10626eedea9
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-18 20:01:23 +0530

    check1

--- backend/api/post/serializers.py
+++ backend/api/post/serializers.py
@@ -10,9 +10,15 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner', 'owner_username', 'text', 'media', 'created_at']
         read_only_fields = ['owner', 'created_at']
 
+
     def to_representation(self, instance):
         """ Add media URL formatting while keeping field writeable """
         rep = super().to_representation(instance)
         if rep.get("media"):
             rep["media"] = rep["media"].replace("http://", "https://")
         return rep
+
+    def get_media(self, obj):
+        if obj.media:
+            return obj.media.url.replace("http://", "https://")
+        return None
