ba5c74c94edf9000309f8d56b61d42f35fffc395
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-19 06:10:05 +0530

    added  pagination

--- backend/api/userbio/models.py
+++ backend/api/userbio/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 from cloudinary.models import CloudinaryField
 
+from django.contrib.auth.models import User
 
 # Create your models here.
 class userbio(models.Model):
     media = CloudinaryField('media', blank=True, null=True)
-    user = models.OneToOneField('auth.user',on_delete=models.CASCADE,related_name='usebio')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=1500,blank=True,null=True)
     college_name = models.CharField(max_length=150,blank=True,null=True)
     college_year = models.CharField(max_length=4,blank=True,null=True)
