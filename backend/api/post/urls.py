316dbcbb6de022682ab481eb10b629f0f243162e
tarunsingh046 <tarunworkonly@gmail.com>
2025-07-20 22:06:42 +0530

    added comment and like

--- backend/api/post/urls.py
+++ backend/api/post/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet ,basename='post')
+router.register(r'comments', CommentViewSet )
 urlpatterns = [
     path('', include(router.urls)),
+    path('post/', include(router.urls)),
 ]
